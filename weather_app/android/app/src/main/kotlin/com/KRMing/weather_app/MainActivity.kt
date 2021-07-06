1ba6b92e70626856bf02b967a1e6f34803dcf72a
KRMing <mkleeboy3@naver.com>
2021-07-06 15:23:42 +0900

    starting weather app

---
+++ weather_app/android/app/src/main/kotlin/com/KRMing/weather_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.KRMing.weather_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ weather_app/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(WeatherApp());
+}
+
+class WeatherApp extends StatefulWidget {
+  const WeatherApp({Key? key}) : super(key: key);
+
+  @override
+  _WeatherAppState createState() => _WeatherAppState();
+}
+
+class _WeatherAppState extends State<WeatherApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
